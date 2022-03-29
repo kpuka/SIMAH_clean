4bc5ce231d54466d7433d097692846d06d8b38c4
Klajdi Puka <klajdi.puka@gmail.com>
2022-03-29 11:47:20 -0400

    added function to format causal mediation results

--- Puka_2022_SES_x_Lifestyles/5_Causal Mediation analysis.R
+++ Puka_2022_SES_x_Lifestyles/5_Causal Mediation analysis.R
@@ -10,6 +10,8 @@ library(tidyverse)  # data management
 library(timereg)    # additive survival models
 library(VGAM)       # multinomial regression, needed for causal mediation
 library(MASS)       # needed for causal mediation functions
+library(knitr)      # formatted table
+
 
 
 # Specify the data and output file locations
@@ -41,135 +43,6 @@ nhis_female <- readRDS (file.path(data, "nhis_female.rds"))
 # Lange et al. 2011 https//doi.org/10.1097/EDE.0b013e31821c680c
 
 
-# Create function for data preparation, to repeat analyses for men and women ------------------------------------------
-causal_mediation_prep <-function(data) {
-
-      # Data Preparation -----------------------------------------------------------------------------------------------------------------------------
-      
-      ### Step 0: Select data to use *****************************************************************************************************************
-      # **********************************************************************************************************************************************
-      mydata <- data %>%
-        mutate(A.edu = edu,
-          M1.alc = alcohol5v2,
-          M2.smk = smoking4,
-          M3.bmi = bmi_cat,
-          M4.phy = phy_act3) %>%
-        dplyr::select(A.edu, M1.alc, M2.smk, M3.bmi, M4.phy, allcause_death, bl_age, end_age, married, ethnicity, srvy_yr)
-      
-      # specifies the reference category
-      mydata$A.edu <- factor(mydata$A.edu, levels=c(1,2,3), labels = c("Low", "Med", "High"))
-      mydata$A.edu <- relevel(mydata$A.edu, ref = "High")
-      
-      # NOTE: For technical reasons, the mediators should be coded as integers starting with 1
-      
-      
-      
-      ### Step 1: Fit a model for each mediator, , conditioning on exposure and all confounders *******************************************************
-      # ***********************************************************************************************************************************************
-      
-      # Fit model for each mediator, conditioning on exposure (education) and all confounders
-      
-      mydata$ATemp <- mydata$A.edu # first, create and use a copy of the exposure variable (for technical reasons related to R)
-      fitM1 <- vglm(M1.alc ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 3))
-      fitM2 <- vglm(M2.smk ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 1))
-      fitM3 <- vglm(M3.bmi ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 2))
-      fitM4 <- vglm(M4.phy ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 3))
-      
-      
-      
-      
-      ### Step 2: Construct copies of ID and exposure *************************************************************************************************
-      # ***********************************************************************************************************************************************
-      
-      #Create ID Variable
-      mydata$ID <- 1:nrow(mydata) # construct id variable
-      
-      # Create counterfactual version of exposure (education); repeated 4 times because there are 4 mediators
-      levelsOfEDU <- unique(mydata$A.edu)
-      myData1 <- mydata
-      myData2 <- mydata
-      myData3 <- mydata
-      myData1$edu_M1.alc <- levelsOfEDU[1]
-      myData2$edu_M1.alc <- levelsOfEDU[2]
-      myData3$edu_M1.alc <- levelsOfEDU[3]
-      tempMyData <- rbind(myData1, myData2, myData3)
-      
-      myData1 <- tempMyData
-      myData2 <- tempMyData
-      myData3 <- tempMyData
-      myData1$edu_M2.smk <- levelsOfEDU[1]
-      myData2$edu_M2.smk <- levelsOfEDU[2]
-      myData3$edu_M2.smk <- levelsOfEDU[3]
-      tempMyData <- rbind(myData1, myData2, myData3)
-      
-      myData1 <- tempMyData
-      myData2 <- tempMyData
-      myData3 <- tempMyData
-      myData1$edu_M3.bmi <- levelsOfEDU[1]
-      myData2$edu_M3.bmi <- levelsOfEDU[2]
-      myData3$edu_M3.bmi <- levelsOfEDU[3]
-      tempMyData <- rbind(myData1, myData2, myData3)
-      
-      myData1 <- tempMyData
-      myData2 <- tempMyData
-      myData3 <- tempMyData
-      myData1$edu_M4.phy <- levelsOfEDU[1]
-      myData2$edu_M4.phy <- levelsOfEDU[2]
-      myData3$edu_M4.phy <- levelsOfEDU[3]
-      newMyData <- rbind(myData1, myData2, myData3)
-      
-      
-      
-      
-      ### Step 3: Construct weights  *********************************************************************************************************************
-      # **************************************************************************************************************************************************
-      
-      # M1: alcohol
-      newMyData$ATemp <- newMyData$A.edu
-      tempDir1 <- as.matrix(predict(fitM1,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M1.alc)]
-      
-      newMyData$ATemp <- newMyData$edu_M1.alc
-      tempIndir1 <- as.matrix(predict(fitM1,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M1.alc)]
-      
-      newMyData$weight1 <- tempIndir1/tempDir1
-      
-      
-      #M2: Smoking
-      newMyData$ATemp <- newMyData$A.edu
-      tempDir2 <- as.matrix(predict(fitM2,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M2.smk)]
-      
-      newMyData$ATemp <- newMyData$edu_M2.smk
-      tempIndir2 <- as.matrix(predict(fitM2,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M2.smk)]
-      
-      newMyData$weight2 <- tempIndir2/tempDir2
-      
-      
-      #M3: BMI
-      newMyData$ATemp <- newMyData$A.edu
-      tempDir3 <- as.matrix(predict(fitM3,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M3.bmi)]
-      
-      newMyData$ATemp <- newMyData$edu_M3.bmi
-      tempIndir3 <- as.matrix(predict(fitM3,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M3.bmi)]
-      
-      newMyData$weight3 <- tempIndir3/tempDir3
-      
-      
-      #M4: Physical activity
-      newMyData$ATemp <- newMyData$A.edu
-      tempDir4 <- as.matrix(predict(fitM4,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M4.phy)]
-      
-      newMyData$ATemp <- newMyData$edu_M4.phy
-      tempIndir4 <- as.matrix(predict(fitM4,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M4.phy)]
-      
-      newMyData$weight4 <- tempIndir4/tempDir4
-      
-      # Final weight
-      newMyData$weightM <- newMyData$weight1 * newMyData$weight2 * newMyData$weight3 * newMyData$weight4
-      
-      return(newMyData)
-}
-    
-
 # Data Preparation ----------------------------------------------------------------------------------------
 causal_mediation_prep(nhis_female) %>% saveRDS(paste0(output, "expandedData_fem.rds"))
 causal_mediation_prep(nhis_male)   %>% saveRDS(paste0(output, "expandedData_male.rds"))
@@ -190,22 +63,14 @@ CMed_f <- aalen(Surv(bl_age, end_age, allcause_death) ~ const(A.edu) * const(edu
                                                         const(married) + factor(ethnicity) + const(factor(srvy_yr)),
                           data=expandedData, weights=expandedData$weightM, clusters=expandedData$ID, robust=0)  
                 
-# Save model results
-saveRDS(CMed_f, file.path(output, "CMed_f.rds"))       
+saveRDS(CMed_f, file.path(output, "CMed_f.rds"))  # Save model results
+     
+# Load model and view results
+CMed_model <-readRDS(file.path(output, "CMed_f.rds"))  # load model (if needed)
+Low_Education <- c(1,3,5,7,9,29,33,37,41)              # List the coefficients of interest for 'low education'
+format_CMed (CMed_model, Low_Education) %>% kable()    # print formatted results
 
 
-# Load model results
-CMed_model <-readRDS(file.path(output, "CMed_f.rds"))  
-
-# The 'summary(model)' command will produce the direct effect, indirect effects, and mediated interaction effects. 
-# Functions were used to extract the other details. 
-              
-# Get final results. NOTE: THE NUMBERS BELOW MAY HAVE TO BE CHANGED IF A DIFFERENT MODEL IS USED
-summary(CMed_model)   
-getTE_NotRobust(CMed_model, c(1,3,5,7,9,29,33,37,41))                             # Function to get the total effect and proportion mediated
-getIE_NotRobust(CMed_model, c(3,5,7,9,29,33,37,41))                               # Function to get the total combined indirect effect  
-getTE_IE_NotRobust(CMed_model, c(1,3,5,7,9,29,33,37,41), c(3,5,7,9,29,33,37,41))  # Function to get the proportion mediated of the combined indirect effect
-      
 
  
      
@@ -224,21 +89,11 @@ CMed_m <- aalen(Surv(bl_age, end_age, allcause_death) ~ const(A.edu) * const(edu
                                                         const(married) + factor(ethnicity) + const(factor(srvy_yr)),
                           data=expandedData, weights=expandedData$weightM, clusters=expandedData$ID, robust=0)
 
-
-# Save model results
-saveRDS(CMed_m, file.path(output, "CMed_m.rds"))       
-
-# Load model results
-CMed_model <-readRDS(file.path(output, "CMed_m.rds"))  
+saveRDS(CMed_m, file.path(output, "CMed_m.rds"))       # Save model results
 
 
-# The 'summary(model)' command will produce the direct effect, indirect effects, and mediated interaction effects. 
-# Functions were used to extract the other details. 
-
-# Get final results. NOTE: THE NUMBERS BELOW MAY HAVE TO BE CHANGED IF A DIFFERENT MODEL IS USED
-summary(CMed_model) 
-getTE_NotRobust(CMed_model, c(1,3,5,7,9,29,33,37,41))                              # Function to get the total effect and proportion mediated
-getIE_NotRobust(CMed_model, c(3,5,7,9,29,33,37,41))                                # Function to get the total combined indirect effect  
-getTE_IE_NotRobust(CMed_model, c(1,3,5,7,9,29,33,37,41), c(3,5,7,9,29,33,37,41))   # Function to get the proportion mediated of the combined indirect effect
-
+# Load model and view results
+CMed_model <-readRDS(file.path(output, "CMed_m.rds"))  # load model (if needed)
+Low_Education <- c(1,3,5,7,9,29,33,37,41)              # List the coefficients of interest for 'low education'
+format_CMed (CMed_model, Low_Education) %>% kable()    # print formatted results
 
--- Puka_2022_SES_x_Lifestyles/Function - CausalMed Results.R
+++ Puka_2022_SES_x_Lifestyles/Function - CausalMed Results.R
@@ -1,7 +1,138 @@
 
-
+# SES x Lifestyle Differential Vulnerability & Exposure Project
 ## Functions for Causal Mediation 
 
+
+# Data preparation for Causal Mediation -------------------------------------------------------------
+causal_mediation_prep <-function(data) {
+  
+  # Data Preparation -----------------------------------------------------------------------------------------------------------------------------
+  
+  ### Step 0: Select data to use *****************************************************************************************************************
+  # **********************************************************************************************************************************************
+  mydata <- data %>%
+    mutate(A.edu = edu,
+      M1.alc = alcohol5v2,
+      M2.smk = smoking4,
+      M3.bmi = bmi_cat,
+      M4.phy = phy_act3) %>%
+    dplyr::select(A.edu, M1.alc, M2.smk, M3.bmi, M4.phy, allcause_death, bl_age, end_age, married, ethnicity, srvy_yr)
+  
+  # specifies the reference category
+  mydata$A.edu <- factor(mydata$A.edu, levels=c(1,2,3), labels = c("Low", "Med", "High"))
+  mydata$A.edu <- relevel(mydata$A.edu, ref = "High")
+  
+  # NOTE: For technical reasons, the mediators should be coded as integers starting with 1
+  
+  
+  
+  ### Step 1: Fit a model for each mediator, , conditioning on exposure and all confounders *******************************************************
+  # ***********************************************************************************************************************************************
+  
+  # Fit model for each mediator, conditioning on exposure (education) and all confounders
+  
+  mydata$ATemp <- mydata$A.edu # first, create and use a copy of the exposure variable (for technical reasons related to R)
+  fitM1 <- vglm(M1.alc ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 3))
+  fitM2 <- vglm(M2.smk ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 1))
+  fitM3 <- vglm(M3.bmi ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 2))
+  fitM4 <- vglm(M4.phy ~ ATemp + bl_age + married + factor(ethnicity) + factor(srvy_yr), data = mydata, family=multinomial(refLevel = 3))
+  
+  
+  
+  
+  ### Step 2: Construct copies of ID and exposure *************************************************************************************************
+  # ***********************************************************************************************************************************************
+  
+  #Create ID Variable
+  mydata$ID <- 1:nrow(mydata) # construct id variable
+  
+  # Create counterfactual version of exposure (education); repeated 4 times because there are 4 mediators
+  levelsOfEDU <- unique(mydata$A.edu)
+  myData1 <- mydata
+  myData2 <- mydata
+  myData3 <- mydata
+  myData1$edu_M1.alc <- levelsOfEDU[1]
+  myData2$edu_M1.alc <- levelsOfEDU[2]
+  myData3$edu_M1.alc <- levelsOfEDU[3]
+  tempMyData <- rbind(myData1, myData2, myData3)
+  
+  myData1 <- tempMyData
+  myData2 <- tempMyData
+  myData3 <- tempMyData
+  myData1$edu_M2.smk <- levelsOfEDU[1]
+  myData2$edu_M2.smk <- levelsOfEDU[2]
+  myData3$edu_M2.smk <- levelsOfEDU[3]
+  tempMyData <- rbind(myData1, myData2, myData3)
+  
+  myData1 <- tempMyData
+  myData2 <- tempMyData
+  myData3 <- tempMyData
+  myData1$edu_M3.bmi <- levelsOfEDU[1]
+  myData2$edu_M3.bmi <- levelsOfEDU[2]
+  myData3$edu_M3.bmi <- levelsOfEDU[3]
+  tempMyData <- rbind(myData1, myData2, myData3)
+  
+  myData1 <- tempMyData
+  myData2 <- tempMyData
+  myData3 <- tempMyData
+  myData1$edu_M4.phy <- levelsOfEDU[1]
+  myData2$edu_M4.phy <- levelsOfEDU[2]
+  myData3$edu_M4.phy <- levelsOfEDU[3]
+  newMyData <- rbind(myData1, myData2, myData3)
+  
+  
+  
+  
+  ### Step 3: Construct weights  *********************************************************************************************************************
+  # **************************************************************************************************************************************************
+  
+  # M1: alcohol
+  newMyData$ATemp <- newMyData$A.edu
+  tempDir1 <- as.matrix(predict(fitM1,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M1.alc)]
+  
+  newMyData$ATemp <- newMyData$edu_M1.alc
+  tempIndir1 <- as.matrix(predict(fitM1,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M1.alc)]
+  
+  newMyData$weight1 <- tempIndir1/tempDir1
+  
+  
+  #M2: Smoking
+  newMyData$ATemp <- newMyData$A.edu
+  tempDir2 <- as.matrix(predict(fitM2,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M2.smk)]
+  
+  newMyData$ATemp <- newMyData$edu_M2.smk
+  tempIndir2 <- as.matrix(predict(fitM2,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M2.smk)]
+  
+  newMyData$weight2 <- tempIndir2/tempDir2
+  
+  
+  #M3: BMI
+  newMyData$ATemp <- newMyData$A.edu
+  tempDir3 <- as.matrix(predict(fitM3,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M3.bmi)]
+  
+  newMyData$ATemp <- newMyData$edu_M3.bmi
+  tempIndir3 <- as.matrix(predict(fitM3,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M3.bmi)]
+  
+  newMyData$weight3 <- tempIndir3/tempDir3
+  
+  
+  #M4: Physical activity
+  newMyData$ATemp <- newMyData$A.edu
+  tempDir4 <- as.matrix(predict(fitM4,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M4.phy)]
+  
+  newMyData$ATemp <- newMyData$edu_M4.phy
+  tempIndir4 <- as.matrix(predict(fitM4,type = "response", newdata=newMyData))[cbind(1:nrow(newMyData),newMyData$M4.phy)]
+  
+  newMyData$weight4 <- tempIndir4/tempDir4
+  
+  # Final weight
+  newMyData$weightM <- newMyData$weight1 * newMyData$weight2 * newMyData$weight3 * newMyData$weight4
+  
+  return(newMyData)
+}
+
+
+# Extract Data from Causal Mediation ----------------------------------------------------------------
 
 # Direct, indirect and mediated interactive effects and standard errors are derived directly from the summary() command below
 # Total effect is obtained by the sum of the three separate effects
@@ -21,8 +152,6 @@ getTE_NotRobust <- function(CMed_model, v){
   rownames(output) <- c(rownames(CMed_model$gamma)[v],"TE")
   return(output)}
 
-
-
         # # Function to get the (Robust) total effect and proportion mediated
         getTE_Robust <- function(CMed_model, v){
           TE <- sum(CMed_model$gamma[v])
@@ -36,9 +165,6 @@ getTE_NotRobust <- function(CMed_model, v){
           colnames(output) <- c("Est.", "SE", "med_prop", "lowerCI", "UpperCI")
           rownames(output) <- c(rownames(CMed_model$gamma)[v],"TE")
           return(output)}
-
-
-
 
 
 # Function to get the (Not-Robust) total combined indirect effect  
@@ -56,8 +182,6 @@ getIE_NotRobust <- function(CMed_model, v){
   rownames(output) <- c(rownames(CMed_model$gamma)[v],"IE")
   return(output)}
 
-
-
         # Function to get the (Robust) total combined indirect effect  
         getIE_Robust <- function(CMed_model, v){
           IE <- sum(CMed_model$gamma[v])
@@ -72,7 +196,6 @@ getIE_NotRobust <- function(CMed_model, v){
           colnames(output) <- c("Est.", "SE", "med_prop", "lowerCI", "UpperCI")
           rownames(output) <- c(rownames(CMed_model$gamma)[v],"IE")
           return(output)}
-
 
 
 
@@ -100,9 +223,7 @@ getTE_IE_NotRobust <- function(CMed_model, v, z){
   output <- cbind(IE, med_prop, quantile)
   return(output)}
 
-
       
-
       # Function to get the (Not Robust)  proportion mediated of the combined indirect effect
       getTE_IE_Robust <- function(CMed_model, v, z){
         #total effect
@@ -124,3 +245,94 @@ getTE_IE_NotRobust <- function(CMed_model, v, z){
         quantile <- quantile(med_prop_CI, c(0.025, 0.975))
         output <- cbind(IE, med_prop, quantile)
         return(output)}
+
+      
+      
+# Format Data from Causal Mediation ----------------------------------------------------------------
+
+format_CMed <- function (model, coef_list) {
+  
+  group <- enexpr(coef_list)
+  
+  # All_coef model coefficients   
+  All_coef <- coef(model) %>%
+    as.data.frame() %>% 
+    rownames_to_column(var = "variable") %>% 
+    mutate (deaths_10000py = round(Coef. * 10000, 1),
+      lower = round(`lower2.5%` * 10000, 1),
+      upper = round(`upper97.5%` * 10000, 1),
+      deaths_10000py_CI = paste0(deaths_10000py, " (", lower, ", ", upper, ")")) %>% 
+    dplyr::select (variable, deaths_10000py_CI)
+  
+  coef <- slice(All_coef, coef_list)
+  
+  
+  # Function to get the total effect and proportion mediated
+  TE_prop <- getTE_NotRobust(model, coef_list) %>% 
+    as.data.frame() %>% rownames_to_column(var = "variable") 
+  
+  TE <- filter(TE_prop, variable =="TE") %>% 
+    mutate (lower = round((`Est.` - (1.96 * SE))*10000,1), 
+      upper = round((`Est.` + (1.96 * SE))*10000,1), 
+      deaths_10000py = round(`Est.` * 10000,1),
+      deaths_10000py_CI = paste0(deaths_10000py, " (", lower, ", ", upper, ")")) %>%
+    dplyr::select(variable, deaths_10000py_CI)
+  
+  prop <- filter(TE_prop, variable !="TE") %>%
+    mutate(prop = round(med_prop * 100,0),
+      lower = round(lowerCI * 100,0),
+      upper = round(UpperCI * 100,0),
+      prop_CI = paste0(prop, " (", lower, ", ", upper, ")")) %>% 
+    dplyr::select(variable, prop_CI) 
+  
+  
+  # Function to get the total combined indirect effect  (coef_list excluding 1st item)
+  IE <- getIE_NotRobust(model, coef_list[-1]) %>% 
+    as.data.frame() %>% rownames_to_column(var = "variable") %>% 
+    filter (variable == "IE") %>% 
+    mutate (lower = round((`Est.` - (1.96 * SE))*10000,1), 
+      upper = round((`Est.` + (1.96 * SE))*10000,1), 
+      deaths_10000py = round(`Est.` * 10000,1),
+      deaths_10000py_CI = paste0(deaths_10000py, " (", lower, ", ", upper, ")")) %>%
+    dplyr::select(variable, deaths_10000py_CI) 
+  
+  
+  # Function to get the proportion mediated of the combined indirect effect
+  IE_prop <- getTE_IE_NotRobust(CMed_model, coef_list, coef_list[-1]) %>% 
+    as.data.frame() %>% rownames_to_column(var = "variable") %>%
+    pivot_wider(names_from="variable", values_from=c("IE", "med_prop", "quantile")) %>%
+    mutate (IE_prop = round(`med_prop_2.5%` * 100, 0),
+      lower = round(`quantile_2.5%` *100, 0), 
+      upper = round(`quantile_97.5%` *100, 0),
+      prop_CI = paste0(IE_prop, " (", lower, ", ", upper, ")"),
+      variable = "IE") %>% 
+    dplyr::select(variable, prop_CI) 
+  
+  
+  one <- full_join(coef, prop, by="variable") %>%
+    mutate(label = c(paste0("02 Direct effect of ", group),
+      "04 Alcohol use: differential exposure",
+      "06 Smoking: differential exposure",
+      "08 BMI: differential exposure",
+      "10 Physical activity: differential exposure", 
+      "05 Alcohol use: differential vulnerability ",
+      "07 Smoking: differential vulnerability ",
+      "09 BMI: differential vulnerability ",
+      "11 Physical activity: differential vulnerability ")) %>% 
+    dplyr::select(label, deaths_10000py_CI, prop_CI) 
+  
+  two <- TE %>%
+    mutate (label = ifelse(variable == "TE", paste0("01 Total effect of ", group), NA),
+      prop_CI = "100") %>% 
+    dplyr::select(label, deaths_10000py_CI, prop_CI)
+  
+  three <- full_join(IE, IE_prop, by="variable") %>%
+    mutate (label = ifelse(variable == "IE", paste0("03 Indirect effect of ", group), NA)) %>% 
+    dplyr::select(label, deaths_10000py_CI, prop_CI) 
+  
+  
+  final <- rbind(one, two, three) %>%
+    arrange(label) 
+  
+  return(final)
+}
